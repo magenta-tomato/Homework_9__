2d2f2b867d71885ed0ac6c9acf40d1c47efbe258
magenta-tomato <magenta.tomato@gmail.com>
2023-10-27 15:20:59 +0300

    version 1

--- lib/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: D:/projects/OTUS/Lesson 2/Homework_2/lib
-# Build directory: D:/projects/OTUS/Lesson 2/Homework_2/lib
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- src/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: D:/projects/OTUS/Lesson 2/Homework_2/src
-# Build directory: D:/projects/OTUS/Lesson 2/Homework_2/src
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(test_lib_functions "test_lib_functions")
-set_tests_properties(test_lib_functions PROPERTIES  _BACKTRACE_TRIPLES "D:/projects/OTUS/Lesson 2/Homework_2/src/CMakeLists.txt;81;add_test;D:/projects/OTUS/Lesson 2/Homework_2/src/CMakeLists.txt;0;")
