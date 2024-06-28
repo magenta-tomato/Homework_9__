a173785570168264cdd7b696f9749156c12131e8
magenta-tomato <91065723+magenta-tomato@users.noreply.github.com>
2024-06-28 23:49:27 +0300

    Update main.cpp

--- app/main.cpp
+++ app/main.cpp
@@ -1,11 +1,19 @@
 
-#include "Bulk.h"
+#include "async.h"
 
 int main(int argc, char* argv[]) 
 {
     int sz = std::stoi(argv[1]);
-
-    Bulk b(sz);
-    b.processLoop();
+    
+    std::size_t bulk = 5;
+    auto h = async::connect(bulk);
+    auto h2 = async::connect(bulk);
+    async::receive(h, "1", 1);
+    //async::receive(h2, "1\n", 2);
+    async::receive(h, "\n2\n3\n4\n5\n6\n{\na\n", 15);
+    async::receive(h, "b\nc\nd\n}\n89\n", 11);
+    async::disconnect(h);
+    async::disconnect(h2);
+    async::wait();
 
 }
